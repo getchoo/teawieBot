448bba80d1140224e982ae98f2052f03b535c284
seth <getchoo@tuta.io>
2022-12-25 17:48:43 -0500

    fix: don't use relative imports

--- src/moyaiBot/moyaiBot.py
+++ src/moyaiBot/moyaiBot.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-from .lib import get_random_response
+from moyaiBoty.lib import get_random_response
 
 intents = discord.Intents.default()
 intents.message_content = True
