2695975f88bc801572f1e526e430bb0748a74ef8
hisashiburi <tophersurp@gmail.com>
2022-12-25 19:11:23 -0800

    add feat: added copypastas into the responses

--- src/moyaiBot/lib.py
+++ src/moyaiBot/lib.py
@@ -15,6 +15,8 @@ def get_random_response(moyai):
 	    "moyai",
 	    "i like y***",
 	    "fard",
+		"OH MY GOD ITS 3 IN THE MORNING AND IM IN MCDONALDS AND WE JUST FOUND OUT THAT WHEN U PULL UP IN MCDONALDS AT 3 AM YOU CAN BUY THE AMONG US HAPPY MEAL WITH A TOY IN IT WHICH IS EITHER THE IMPOSTOR OR THE CREWMATE AND IF YOU DONT KNOW WHAT AMONG US IS YOU MUST BE MUST REALLY BE LIVING UNDER A ROCK ITS AN AWESOME GAME WITH IMPOSTORS AND CREWMATES AND BASICALLY THE IMPOSTOR TRIES TO SABOTAGE THE WHOLE GAME AND THE CREWMATES NEED TO STOP HIM BUT APPARENTLY WHEN YOU PURCHASE THE AMONG US HAPPY MEAL SOMETHING SCARY HAPPENS",
+        "Tick-tock \n Heavy like a Brinks truck \n Looking like I'm tip-top \n Shining like a wristwatch \n Time will grab your wrist \n Lock it down 'til the thing pop \n Can you stick around for a minute 'til the ring stop? \n Please, God",
 	    str(discord.utils.get(moyai.emojis, name="moyai")),
 	]
 	return random.choice(responses)
