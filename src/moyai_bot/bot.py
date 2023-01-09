a00262ac5a79360a9466b464173af24db5006148
seth <getchoo@tuta.io>
2023-01-08 21:21:04 -0500

    feat: add copypasta command

--- src/moyai_bot/bot.py
+++ src/moyai_bot/bot.py
@@ -1,8 +1,10 @@
 import discord
+from discord import app_commands
 from discord.ext import commands
 
-from moyai_bot.lib import get_random_response
+from moyai_bot.lib import get_copypasta, get_random_response
 
+SERVER_ID = discord.Object(id=1055663552679137310)
 intents = discord.Intents.default()
 intents.message_content = True  # pylint: disable=assigning-non-slot
 moyai = commands.Bot(command_prefix="m!", description="moyai", intents=intents)
@@ -11,6 +13,8 @@ moyai = commands.Bot(command_prefix="m!", description="moyai", intents=intents)
 @moyai.event
 async def on_ready():
 	print(f"logged in as {moyai.user}")
+	await moyai.tree.sync(guild=SERVER_ID)
+	print("ready!")
 
 
 @moyai.event
@@ -41,3 +45,17 @@ async def moyaispam(ctx: commands.Context):
 	for _ in range(30):
 		msg += str(discord.utils.get(moyai.emojis, name="moyai"))
 	await ctx.send(msg)
+
+
+@moyai.tree.command(name="copypasta",
+                    description="send funni copypasta",
+                    guild=SERVER_ID)
+@app_commands.choices(choices=[
+    app_commands.Choice(name="happymeal", value="happymeal"),
+    app_commands.Choice(name="ismah", value="ismah"),
+    app_commands.Choice(name="sus", value="sus"),
+    app_commands.Choice(name="ticktock", value="ticktock")
+])
+async def copypasta(i: discord.Interaction, choices: app_commands.Choice[str]):
+	msg = get_copypasta(choices.value)
+	await i.response.send_message(msg)
