49623ef77383d3b7c26a8785e7ed50f89f679f48
seth <getchoo@tuta.io>
2023-09-07 16:18:29 -0400

    flake: switch to nixpkgs rust infra

--- parts/derivation.nix
+++ parts/derivation.nix
@@ -1,20 +1,48 @@
 {
   lib,
-  stdenv,
-  craneLib,
+  rustPlatform,
   self,
-  ...
-}:
-craneLib.buildPackage {
-  src = craneLib.cleanCargoSource self;
-  inherit (self.packages.${stdenv.hostPlatform.system}) cargoArtifacts;
+  lto ? true,
+  optimizeSize ? false,
+}: let
+  filter = path: type: let
+    path' = toString path;
+    base = baseNameOf path';
+    parent = baseNameOf (dirOf path');
 
-  meta = with lib; {
-    mainProgram = "teawiebot";
-    description = "funni bot";
-    homepage = "https://github.com/getchoo/teawiebot";
-    license = licenses.mit;
-    platforms = with platforms; unix;
-    maintainers = with maintainers; [getchoo];
-  };
-}
+    dirBlocklist = ["parts"];
+
+    matches = lib.any (suffix: lib.hasSuffix suffix base) [".rs"];
+    isCargo = base == "Cargo.lock" || base == "Cargo.toml";
+    isCopypasta = parent == "copypastas";
+    isAllowedDir = !(builtins.elem base dirBlocklist);
+  in
+    (type == "directory" && isAllowedDir) || matches || isCargo || isCopypasta;
+
+  filterSource = src:
+    lib.cleanSourceWith {
+      src = lib.cleanSource src;
+      inherit filter;
+    };
+in
+  rustPlatform.buildRustPackage {
+    pname = "teawiebot";
+    version = builtins.substring 0 8 self.lastModifiedDate or "dirty";
+
+    src = filterSource self;
+
+    cargoLock.lockFile = ../Cargo.lock;
+
+    RUSTFLAGS =
+      lib.optionalString lto " -C lto=thin -C embed-bitcode=yes"
+      + lib.optionalString optimizeSize " -C codegen-units=1 -C strip=symbols -C opt-level=z";
+
+    meta = with lib; {
+      mainProgram = "teawiebot";
+      description = "funni bot";
+      homepage = "https://github.com/getchoo/teawiebot";
+      license = licenses.mit;
+      platforms = with platforms; unix;
+      maintainers = with maintainers; [getchoo];
+    };
+  }
--- parts/packages.nix
+++ parts/packages.nix
@@ -1,29 +1,14 @@
 {self, ...}: {
   perSystem = {
-    craneLib,
+    lib,
     pkgs,
     system,
     ...
   }: {
-    packages = {
-      cargoArtifacts = craneLib.buildDepsOnly {src = craneLib.cleanCargoSource self;};
-
-      teawiebot = pkgs.callPackage ./derivation.nix {inherit craneLib self;};
-
-      teawiebot-smol =
-        self.packages.${system}.teawiebot.overrideAttrs (_: {
-            # statically link musl, optimize for size
-            CARGO_BUILD_TARGET = "x86_64-unknown-linux-musl";
-
-            CARGO_BUILD_RUSTFLAGS = "-C lto=fat -C embed-bitcode=yes \
-									-C target-feature=+crt-static -C opt-level=z -C strip=symbols -C codegen-units=1";
-
-            CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_LINKER = let
-              inherit (pkgs.pkgsStatic.stdenv) cc;
-            in "${cc}/bin/${cc.targetPrefix}cc";
-          });
-
+    packages = lib.fix (f: {
+      teawiebot = pkgs.callPackage ./derivation.nix {inherit self;};
+      teawiebot-smol = f.teawiebot.override {optimizeSize = true;};
       default = self.packages.${system}.teawiebot;
-    };
+    });
   };
 }
