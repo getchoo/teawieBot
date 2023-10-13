1a173b6e696be1711fbdb46e0039ea4aaaa8e260
uku <uku3lig@gmail.com>
2023-10-13 18:31:21 +0200

    move client & logic into api module

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,1 +1,11 @@
+use once_cell::sync::Lazy;
+
 pub mod guzzle;
+pub mod shiggy;
+
+pub static REQWEST_CLIENT: Lazy<reqwest::Client> = Lazy::new(|| {
+	reqwest::Client::builder()
+		.user_agent("teawieBot/0.1.0")
+		.build()
+		.unwrap_or_default()
+});
