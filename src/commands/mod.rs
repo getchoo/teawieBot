9dc30519d43553bfd41fc1acfa48efc62ad6fa58
seth <getchoo@tuta.io>
2023-11-15 23:43:28 -0500

    chore: separate command declaration from main.rs

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,6 +1,5 @@
 pub mod ask;
 pub mod bing;
-pub mod bottom;
 pub mod convert;
 pub mod copypasta;
 pub mod random_lore;
@@ -8,3 +7,24 @@ pub mod random_shiggy;
 pub mod random_teawie;
 pub mod teawiespam;
 pub mod version;
+
+use crate::{Data, Error};
+use poise::Command;
+
+pub fn to_global_commands() -> Vec<Command<Data, Error>> {
+	vec![
+		ask::ask(),
+		bing::bing(),
+		convert::convert(),
+		random_lore::random_lore(),
+		random_shiggy::random_shiggy(),
+		random_teawie::random_teawie(),
+		copypasta::copypasta(),
+		teawiespam::teawiespam(),
+		version::version(),
+	]
+}
+
+pub fn to_guild_commands() -> Vec<Command<Data, Error>> {
+	vec![copypasta::copypasta(), teawiespam::teawiespam()]
+}
