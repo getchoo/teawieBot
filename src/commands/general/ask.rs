cc35ac5be15a3b20fd02c2a5d4a499c41f9d6bc9
seth <getchoo@tuta.io>
2023-12-15 16:20:51 -0500

    commands: fix question param in ask

--- src/commands/general/ask.rs
+++ src/commands/general/ask.rs
@@ -6,7 +6,9 @@ use color_eyre::eyre::{Context as _, Result};
 #[poise::command(prefix_command, slash_command)]
 pub async fn ask(
 	ctx: Context<'_>,
-	#[description = "The question you want to ask teawie"] _question: String,
+	#[rename = "question"]
+	#[description = "The question you want to ask teawie"]
+	_question: String,
 ) -> Result<()> {
 	let resp = utils::random_choice(consts::RESPONSES)
 		.wrap_err("Couldn't choose from random responses!")?;
